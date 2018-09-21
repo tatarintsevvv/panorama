10a43ec7406d38cdb9f53bd94c0377df2c8359c3
tatarintsevvv <tatarintsev_vv@mail.ru>
2018-09-21 19:31:04 +0300

    Fix #4

--- upload_file.php
+++ upload_file.php
@@ -40,7 +40,7 @@ function search_file($folderName, $extensions)
     // открываем текущую папку 
     $dir = opendir($folderName); 
     // перебираем папку 
-    while (($file = readdir($dir)) !== false){ // перебираем пока есть файлы
+    while (($file = readdir($dir)) !== false) { // перебираем пока есть файлы
         if ($file != "." && $file != "..") { // если это не папка
             if (is_file($folderName."/".$file)) { // если файл проверяем имя
                 // если имя файла нужное, то вернем путь до него
